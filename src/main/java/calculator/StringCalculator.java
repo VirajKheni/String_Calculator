b7f7b182cafcf0405359c3d560593439c6992cc7
VirajKheni <69363980+VirajKheni@users.noreply.github.com>
2021-08-30 06:33:12 +0530

    Commit Test String Is Empty

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,5 +2,15 @@ package calculator;
 
 public class StringCalculator {
 
+    public static int add(String text) {
+        if (checkStringIsEmpty(text)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    private static boolean checkStringIsEmpty(String text) {
+        return text.isEmpty();
+    }
 
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,19 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+
+    @Test
+    @DisplayName("When string is Empty")
+    void stringIsEmptyTest() {
+        int actual = StringCalculator.add("");
+        int expected = 0;
+        assertEquals(expected, actual , () -> "Should return "+ expected + " but returned " + actual);
+    }
 
 
 }
