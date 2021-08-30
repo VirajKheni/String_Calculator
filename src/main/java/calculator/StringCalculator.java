b08ae81c8c13ccceb040bb46d309f8181ead66ea
VirajKheni <69363980+VirajKheni@users.noreply.github.com>
2021-08-30 06:43:42 +0530

    Commit Test String is Two Number Delimiter by Comma

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,16 +1,54 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public static int add(String text) {
         if (checkStringIsEmpty(text)) {
             return 0;
+        }else {
+            ArrayList<Integer> numbers = stringSplitByDelimiter(text);
+            return sumOfAllNumbers(numbers);
         }
-        return 1;
     }
 
     private static boolean checkStringIsEmpty(String text) {
         return text.isEmpty();
+    }
+
+    private static ArrayList<Integer> stringSplitByDelimiter(String text) {
+        List<String> elements;
+
+        elements = Arrays.asList(stringSplitByCommaOrNewLineDelimiter(text));
+
+        return toInteger(elements);
+    }
+
+    private static String[] stringSplitByCommaOrNewLineDelimiter(String text) {
+        return text.split("[,\n]");
+    }
+
+    private static int toInteger(String text) throws NumberFormatException {
+        return Integer.parseInt(text);
+    }
+
+    private static ArrayList<Integer> toInteger(List<String> elements) throws NumberFormatException {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(String s: elements)
+            numbers.add(StringCalculator.toInteger(s));
+        return numbers;
+    }
+
+    private static int sumOfAllNumbers(ArrayList<Integer> numbers) {
+        int sum=0;
+        for(int number : numbers) {
+            if(number > 1000) continue;
+            sum +=number;
+        }
+        return sum;
     }
 
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,5 +25,16 @@ public class StringCalculatorTest {
         assertEquals(expected, actual , () -> "Should return "+ expected + " but returned " + actual);
     }
 
-
+    @Nested
+    @DisplayName("When string is Comma or NewLine Delimiter")
+    @Tag("CustomDelimiter")
+    class CommaNewLineDelimiter {
+        @Test
+        @DisplayName("When string is Two Number Delimiter by Comma")
+        void stringIsTwoNumberSepByCommaTest() {
+            int actual = StringCalculator.add("1,2");
+            int expected = 3;
+            assertEquals(expected, actual, () -> "Should return the sum of two number which delimited by a comma is " + expected + " but returned " + actual);
+        }
+    }
 }
